7c627bbc5a9329be6c4d89eba7388298eb3ccc1e
nacht29 <nacht29.studygmail.com>
2025-03-11 13:53:05 +0800

    feat: automated buckt to drive script

--- bucket-gdrive/bucket-pipeline.py
+++ bucket-gdrive/bucket-pipeline.py
@@ -1,24 +1,34 @@
+import pendulum
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.models.param import Param
+from airflow.utils.log.logging_mixin import LoggingMixin
+
 import os
 import warnings
 import subprocess
 import pandas as pd
-from datetime import date
+from datetime import date, datetime
 from google.cloud import bigquery as bq
 from google.cloud import storage
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from google.api_core.exceptions import Forbidden, NotFound
 
-# print("Current working directory:", os.getcwd())
-# print("Files in current directory:", os.listdir('sql-scripts'))
+TIME_ZONE = pendulum.timezone('Asia/Singapore')
+START_DATE = datetime(2025, 3, 9, tzinfo=TIME_ZONE)
+
+SQL_SCRIPTS_PATH = 'sql-scripts/'
+JSON_KEYS_PATH = 'json-keys/'
 
 # Service Account JSON
-BQ_SERVICE_ACCOUNT = 'json-keys/explore29-5a4f7581e39f.json'
-BUCKET_SERVICE_ACCOUNT = 'json-keys/test_bucket_gdrive.json'
+BQ_SERVICE_ACCOUNT = f'{JSON_KEYS_PATH}explore29-5a4f7581e39f.json'
+BUCKET_SERVICE_ACCOUNT = f'{JSON_KEYS_PATH}test_bucket_gdrive.json'
 
 # Google Drive auth
 SCOPES = ['https://www.googleapis.com/auth/drive']
 PARENT_FOLDER_ID = '1OLll_SpLxMhVC5kY3ISu0lf0cdF01_pP'
+
 
 warnings.filterwarnings('ignore')
 
@@ -32,8 +42,15 @@ bucket_key_path = BUCKET_SERVICE_ACCOUNT
 bucket_credentials = service_account.Credentials.from_service_account_file(bucket_key_path)
 bucket_client = storage.Client(credentials=bucket_credentials, project=bucket_credentials.project_id)
 
-def file_type_in_dir(file_type:str):
-	files_in_dir = os.listdir('sql-scripts')
+def file_type_in_dir(dir:str, file_type:str):
+	if dir is None:
+		files_in_dir = os.listdir()
+	else:
+		files_in_dir = os.listdir(dir)
+	
+	if file_type is None:
+		return(files_in_dir)
+
 	files = [file for file in files_in_dir if file.endswith(file_type)]
 	return files
 
@@ -42,12 +59,12 @@ def gen_file_name(file:str, file_type:str, replace_type:str):
 	return file_name
 
 def query_data():
-	files_in_dir = os.listdir('sql-scripts')
+	files_in_dir = os.listdir(SQL_SCRIPTS_PATH)
 
-	sql_scripts = file_type_in_dir('.sql')
+	sql_scripts = file_type_in_dir(SQL_SCRIPTS_PATH, '.sql')
 
 	for script in sql_scripts:
-		with open(script, 'r') as cur_script:
+		with open(f'{SQL_SCRIPTS_PATH}{script}', 'r') as cur_script:
 			query = ' '.join([line for line in cur_script])
 			out_file = gen_file_name(script, '.sql', '.csv')
 			results = bq_client.query(query).to_dataframe()
@@ -56,7 +73,7 @@ def query_data():
 def load_bq():
 	bucket = bucket_client.get_bucket('bucket_drive_test')
 
-	load_files = file_type_in_dir('.csv')
+	load_files = file_type_in_dir(None, '.csv')
 	for file in load_files:
 		bucket.blob(file).upload_from_filename(file)
 
@@ -72,7 +89,7 @@ def load_gdrive():
 	response = service.files().list(q=query, fields='files(id, name)').execute()
 	files_in_drive = response.get('files_in_drive') # response.get('files', [])
 
-	load_files = file_type_in_dir('.csv')
+	load_files = file_type_in_dir(None, '.csv')
 	for load_file in load_files:
 		query = f"'{PARENT_FOLDER_ID}' in parents and name='{load_file}' and trashed=false"
 		response = service.files().list(q=query, fields='files(id, name)').execute()
@@ -92,13 +109,44 @@ def load_gdrive():
 			media_body=load_file
 		).execute()
 
-def main():
-	query_data()
-	load_bq()
-	load_gdrive()
+def remove_outfiles():
+	files_in_dir = file_type_in_dir(None, '.csv')
 
-	rm_files = file_type_in_dir('.csv')
-	for file in rm_files:
+	for file in files_in_dir:
 		os.remove(file)
+	print(files_in_dir)
 
-main()
+with DAG(
+	'bucket_pipeline',
+	start_date=START_DATE,
+	schedule="51 13 * * *",
+	catchup=True
+) as dag:
+
+	task_query_data = PythonOperator(
+		task_id='Get_data_from_BigQuery',
+		python_callable=query_data,
+		provide_context=True
+	)
+
+	task_load_bq = PythonOperator(
+		task_id='Load_files_to_BQ',
+		python_callable=load_bq,
+		provide_context=True
+	)
+
+	task_load_gdrive = PythonOperator(
+		task_id='Load_files_to_Drive',
+		python_callable=load_gdrive,
+		provide_context=True
+	)
+
+	task_remove_outfiles = PythonOperator(
+		task_id='Remove_out_files',
+		python_callable=remove_outfiles,
+		provide_context=True
+	)
+
+	task_query_data >> task_load_bq
+	task_load_bq >> task_load_gdrive
+	task_load_gdrive >> task_remove_outfiles
